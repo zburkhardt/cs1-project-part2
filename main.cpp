6b26c05e434d3485bf73c5e4afea0e2334af1a14
Zach Burkhardt <zburkhardt18@gmail.com>
2018-10-21 17:58:17 -0400

    Decomposed transportation menu, transportation selection confirmations, and route menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
     COPYRIGHT (C) 2018 Zach Burkhardt (zmb14) All rights reserved.
-    CSI Project - Part 1
+    CSI Project - Part 2
     Author. Zach Burkhardt
             zmb14@zips.uakron.edu
-    Version. 1.00 09.29.2018
+    Version. 2.00 11.04.2018
     Purpose: This program will tell a user the time to travel based on selected location and transportation method.
 */
 
@@ -31,6 +31,10 @@ const float CLE_ORD = 273.00f,
             JFK_MIA = 949.00f,
             DFW_CYYZ = 1041.00f;
 
+void transMenu(int &selection);
+void transConfirm(int &selection);
+void routeMenu(int &selection);
+
 int main()
 {
     bool loopProgram = true; // loop program until quit from menu
@@ -42,39 +46,14 @@ int main()
     do
     {
 
-        cout << "Welcome to Zach Burkhardt Airlines" << endl;
+    int transSel = 0, routeSel = 0;
 
-        int transSel = 0;
-        bool invalidInput = true;
-        do // Main Menu, choose transportation method
-        {
+    transMenu(transSel);
+    transConfirm(transSel);
+    routeMenu(routeSel);
 
-            cout << endl;
-            cout << "  --------------------------------------------" << endl;
-            cout << " |               1) Helicopter                |" << endl;
-            cout << " |               2) Turbo Prop                |" << endl;
-            cout << " |               3) Midsize Jet               |" << endl;
-            cout << " |               4) Heavy Jet                 |" << endl;
-            cout << " |               5) QUIT                      |" << endl;
-            cout << "  --------------------------------------------" << endl;
-            cout << endl;
-
-            cout << "Enter the number of your transportation method and press Enter: ";
-            cin >> transSel;
-
-            if((cin.fail()) || (transSel < 1) || (transSel > 5)) // Verifies good input
-            {
-                cout << "*** SELECTION IS INVALID ***";
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                invalidInput = true;
-            }
-            else
-            {
-                invalidInput = false;
-            }
-
-        } while(invalidInput);
+    }while(loopProgram);
+}
 
 
 
@@ -85,7 +64,7 @@ int main()
         {
             cout << "*** HELICOPTER SELECTED ***" << endl;
 
-            int routeSel = 0;
+
             do
             {
 
@@ -1069,4 +1048,104 @@ int main()
 
 return 0;
 
+}
+
+
+void transMenu(int &selection)
+{
+    cout << "Welcome to Zach Burkhardt Airlines" << endl;
+
+    bool invalidInput = true;
+    do // Main Menu, choose transportation method
+    {
+
+        cout << endl;
+        cout << "  --------------------------------------------" << endl;
+        cout << " |               1) Helicopter                |" << endl;
+        cout << " |               2) Turbo Prop                |" << endl;
+        cout << " |               3) Midsize Jet               |" << endl;
+        cout << " |               4) Heavy Jet                 |" << endl;
+        cout << " |               5) QUIT                      |" << endl;
+        cout << "  --------------------------------------------" << endl;
+        cout << endl;
+
+        cout << "Enter the number of your transportation method and press Enter: ";
+        cin >> selection;
+
+        if((cin.fail()) || (selection < 1) || (selection > 5)) // Verifies good input
+        {
+            cout << "*** SELECTION IS INVALID ***";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            invalidInput = true;
+        }
+        else
+        {
+            invalidInput = false;
+        }
+
+    } while(invalidInput);
+}
+
+void transConfirm(int &selection)
+{
+    // Helicopter Branch
+    if(selection == 1)
+    {
+        cout << "*** HELICOPTER SELECTED ***" << endl;
+    }
+
+    // Turbo Prop Branch
+    else if(transSel == 2)
+    {
+    cout << "*** TURBO PROP SELECTED ***" << endl;
+    }
+
+    // Midsize Jet Branch
+    else if(transSel == 3)
+    {
+    cout << "*** MIDSIZE JET SELECTED ***" << endl;
+    }
+
+    // Heavy Jet Branch
+    else if(transSel == 4)
+    {
+    cout << "*** HEAVY JET SELECTED ***" << endl;
+    }
+}
+
+void routeMenu(int &selection)
+{
+    bool invalidInput = true;
+    do
+    {
+
+        cout << endl;
+        cout << "  --------------------------------------------" << endl;
+        cout << " |                 1) CLE-ORD                 |" << endl;
+        cout << " |                 2) ORD-JFK                 |" << endl;
+        cout << " |                 3) DEN-JFK                 |" << endl;
+        cout << " |                 4) JFK-SFO                 |" << endl;
+        cout << " |                 5) JFK-MIA                 |" << endl;
+        cout << " |                 6) DFW-CYYZ                |" << endl;
+        cout << "  --------------------------------------------" << endl;
+        cout << endl;
+
+        cout << endl;
+        cout << "Enter the number of your route and press Enter: ";
+        cin >> selection;
+
+        if((cin.fail()) || (selection < 1) || (selection > 6))
+        {
+            cout << "*** SELECTION IS INVALID ***" << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            invalidInput = true;
+        }
+        else
+        {
+            invalidInput = false;
+        }
+
+    } while(invalidInput);
 }
